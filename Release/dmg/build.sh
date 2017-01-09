b3c0c2a943eae99cea0a66702a06805af92ef54b
Artem Kirienko <artem.kirienko@gmail.com>
2017-01-09 12:36:05 +0800

    Release and paths updated

--- Release/dmg/build.sh
+++ Release/dmg/build.sh
@@ -1,7 +1,7 @@
 cd "`dirname "$0"`"
 echo removing old files
 echo `find . -name SimSim*.dmg -delete`
-appver="$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString"  "../../Build/Products/Release/SimSim.app/Contents/Info.plist")"
+appver="$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString"  "../../build/SimSim/Build/Products/Release/SimSim.app/Contents/Info.plist")"
 echo building dmg
 appdmg dmg_config.json "SimSim_"$appver".dmg"
 echo dmg done
