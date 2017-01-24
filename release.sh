0cab05a612b2d699bed7b0cc889ce2472fdaa76a
Artem Kirienko <artem.kirienko@gmail.com>
2017-01-24 19:56:08 +0300

    Release script updated
    Usage:
    ./release.sh $VERSION_STRING
    e.g.:
    ./release.sh 1.2.5
    
    You don’t have to do anything else to build a release.

--- release.sh
+++ release.sh
@@ -1,10 +1,13 @@
+NEW_VERSION_STRING=$1
+
 PLIST="./SimSim/Info.plist"
 
-# Increment Build number
+# Increment Build number and update version
 PLB=/usr/libexec/PlistBuddy
 LAST_NUMBER=$($PLB -c "Print CFBundleVersion" "$PLIST")
 NEW_VERSION=$(($LAST_NUMBER + 1))
 $PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST"
+$PLB -c "Set :CFBundleShortVersionString $NEW_VERSION_STRING" $PLIST
 
 # Build
 xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
@@ -13,9 +16,8 @@ xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 ./Release/dmg/build.sh 
 
 # ZIP
-VERSION_STRING=$($PLB -c "Print CFBundleShortVersionString" $PLIST)
 cd ./build/Release/
-zip -r ../../Release/SimSim_${VERSION_STRING}.zip ./SimSim.app
+zip -r ../../Release/SimSim_${NEW_VERSION_STRING}.zip ./SimSim.app
 cd ../../
 
 echo "Done"
